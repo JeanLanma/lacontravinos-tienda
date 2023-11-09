0055eddd504948558e34029af74853c26f33dd00
Jean <jeanmacario048@gmail.com>
2023-11-09 15:42:36 -0600

    Add\ finished main page branches responsive

--- resources/views/frontstore/home-branches.blade.php
+++ resources/views/frontstore/home-branches.blade.php
@@ -8,13 +8,13 @@
             <h1 class="text-2xl md:text-4xl md:text-[2.5rem] font-bold text-white">¡Bienvenido a La Contra, La Tienda del Vino Mexicano!</h1>
         </div>
 
-        <div class="w-full mt-36 text-center flex flex-col justify-center">
+        <div class="w-full mt-12 lg:mt-36 text-center flex flex-col justify-center">
             
             <div class="mb-16">
                 <h3 class="text-xl md:text-4xl font-bold text-white">ELIGE TU CIUDAD MÁS CERCANA</h3>
             </div>
             
-            <div class="flex justify-center">
+            <div class="flex justify-center h-[27rem] md:h-auto">
                 <ul class="flex flex-col md:flex-row gap-6 bg-black absolute px-12 md:px-12 xl:px-24 py-5 text-white font-bold text-2xl">
                     <li class="hover:opacity-70 transition-opacity duration-200">
                         <a href="#">Ensenada</a>
--- resources/views/layouts/frontstore.blade.php
+++ resources/views/layouts/frontstore.blade.php
@@ -25,15 +25,6 @@
                 @include('layouts.navigation-frontstore')
             </div>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
             <!-- Page Content -->
             <main>
                 @yield('content')
